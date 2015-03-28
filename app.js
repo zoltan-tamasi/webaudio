d13a687d1c67ed115879726453b441534abe413c
Zoltan Tamasi <tzoltan84@gmail.com>
2015-03-28 10:37:12 +0100

    Added refreshToken job to main app.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var redis = require('redis');
+var CronJob = require('cron').CronJob;
 
 var redisData = JSON.parse(process.env.VCAP_SERVICES)["redis-2.2"][0];
 var redisStore = redis.createClient(redisData.credentials.port, redisData.credentials.host);
@@ -9,6 +10,62 @@ redisStore.auth(redisData.credentials.password);
 redisStore.on('connect', function() {
     console.log('connected to redis');
 });
+
+var job = new CronJob('0 20 * * * *', refreshToken, null, false, 'Europe/Berlin');
+job.start();
+
+function getDateTime() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
+
+function refreshToken() {
+    redisStore.mget(['oauthToken', 'refreshToken'], function(err, reply) {
+        if (err) { console.log(err); return; }
+        request.post('https://www.freesound.org/apiv2/oauth2/access_token/', {
+            form: {
+                'client_id' : process.env.CLIENT_ID || 'e4cd6c8ea6167f5ae50d',
+                'client_secret' : process.env.CLIENT_SECRET || '8390a6d1330cbb30bb4b79794f3a0a36ed95b877',
+                'grant_type' : 'refresh_token',
+                'refresh_token' : reply[1]
+            }
+        }, function(error, response, body) {
+            var tokenStore = {
+                oauthToken : JSON.parse(response.body).access_token,
+                refreshToken : JSON.parse(response.body).refresh_token
+            };
+
+            console.log(getDateTime()  + ': new access token is:' + tokenStore.oauthToken);
+            console.log(getDateTime() + ': new refresh token is:' + tokenStore.refreshToken);
+
+            if (tokenStore.oauthToken !== undefined) {
+                redisStore.set('oauthToken', tokenStore.oauthToken);
+                redisStore.set('refreshToken', tokenStore.refreshToken);
+                redisStore.set('oauthTokenDateTime', getDateTime());
+            } else {
+                console.log("Retrieving oauthToken failed");
+            }
+        });
+    }); 
+}
 
 app.use(express.static(__dirname));
 
